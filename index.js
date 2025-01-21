21f5429b51b059632c8962be8753048762d4885b
IsmaelNjama <petermailsnjama@gmail.com>
2025-01-21 22:39:43 +0200

    console log edits

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(async (req, res, next) => {
   } catch (error) {
     return next(ERR_UNAUTHORIZED);
   }
-  console.log("middleware working");
+  console.log("The middleware working okay");
   next();
 });
 
